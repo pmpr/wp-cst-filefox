e91277578819138b62b627f6433b658510505879
pmpr.dev <pmpr.dev@gmail.com>
2025-07-31 13:41:45 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             688b675439c02             |
+    |             688b726759c6d             |
     |_______________________________________|
 */
  use Pmpr\Custom\Filefox\Filefox; Filefox::symcgieuakksimmu();
--- src/Pmpr/Custom/Filefox/Frontend/Page/Request/Download.php
+++ src/Pmpr/Custom/Filefox/Frontend/Page/Request/Download.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             688b675439c02             |
+    |             688b726759c6d             |
     |_______________________________________|
 */
  namespace Pmpr\Custom\Filefox\Frontend\Page\Request; use Pmpr\Common\Foundation\Frontend\Page; use Pmpr\Common\Foundation\Interfaces\Constants; use Pmpr\Custom\Filefox\Setting\Setting; class Download extends Page { public function qiccuiwooiquycsg() { $this->wegcaymyqqoyewmw(Constants::kucuwimmakgiieom)->gswweykyogmsyawy(__('Request File', PR__CST__FILEFOX))->wmsaakuicamguoam($this->weysguygiseoukqw(Setting::oeyomkquakmseosk)); } public function rsysgcucogueguuk() : array { return ['track_title' => __('Track Request', PR__CST__FILEFOX), 'track_action' => 'ff_ticket_get_track_form', 'submit_title' => __('Submit Request', PR__CST__FILEFOX), 'submit_action' => 'ff_ticket_get_file_request_form']; } public function enqueue() { $this->ewcsyqaaigkicgse('ticket_enqueue_form_assets'); } }
